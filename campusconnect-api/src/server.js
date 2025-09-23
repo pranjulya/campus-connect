e629169000a2e851ab5039f527be08a62967826e
Pranjulya Bajpai <pranjulyabajpai@Pranjulyas-Mac-mini.local>
2025-09-23 21:10:04 +0530

    enh:/added-application-level-logging

--- campusconnect-api/src/server.js
+++ campusconnect-api/src/server.js
@@ -1,11 +1,12 @@
 import app from './app.js';
 import connectDB from './config/db.js';
 import env from './config/env.js';
+import logger from './config/logger.js';
 
 connectDB();
 
 const port = env.PORT;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  logger.info(`Server is running on port ${port}`);
 });
